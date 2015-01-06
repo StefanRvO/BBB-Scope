ab5884e74744942c817e4112992c317782485241
Stefan Ravn van Overeem <stefan@stefanrvo.dk>
2015-01-06 23:12:34 +0100

    Fixed typos

--- client/SampleSender.cpp
+++ client/SampleSender.cpp
@@ -96,7 +96,7 @@ void SampleSender::sampleSocketThread()
         while(curSamplesindex<1000 and !stop)
         {
             cursamples[curSamplesindex++]=RB->pop_front();
-            while(RB.empty()) t.tick();
+            while(RB->empty()) t.tick();
         }
         curSamplesindex=0;
         sendPointer=0;
@@ -109,18 +109,18 @@ void SampleSender::sampleSocketThread()
                 return;
             }
             sendPointer+=size;
-        }
             sendCounter++;
             if(RB->full() and sendCounter>10)
             {   //adjust speed if network is overloaded
-                sendcounter=9;
+                sendCounter=9;
                 options->sampleTime+=options->sampleTime/1000*3; //go down 0.3%
                 options->sampleTimeMin=options->sampleTime;
                 Adc->resetSampler();
                 controlMessage control;
                 control.changespeed=-1;
                 control.time=options->sampleTime;
-                if ((write(socket_control,(char *)&control, sizeof(controlMessage))== -1)) {
+                if ((write(socket_control,(char *)&control, sizeof(controlMessage))== -1)) 
+                {
                     printf( "Failure Sending Message\n");
                     stop=true;
                     return;
